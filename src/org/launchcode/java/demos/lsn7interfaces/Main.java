07b12ed0d87920b7f59041a82567f4059c9cf603
Shaun Minnihan <spminnihan@gmail.com>
2019-12-02 00:08:27 -0600

    lsn7 exercise complete

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -10,10 +11,19 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+        flavors.sort(new FlavorComparator());
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        cones.sort(new ConeComparator());
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        for(Flavor flavor : flavors) {
+            System.out.println(flavor);
+        }
+
+        for(Cone cone : cones) {
+            System.out.println(cone);
+        }
     }
 }
